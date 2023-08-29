9d6ee6fba9a60a818e243097f87ff9f6d9cdc234
Newton <altontonnalumasa@gmail.com>
2023-08-29 16:33:09 +0300

    Add less view button

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
-const navToggle = document.getElementById('navi-toggle');
+const navToggle = document.getElementById("navi-toggle");
 
 const closeNav = () => {
   navToggle.checked = false;
 };
 
-const closeOne = document.querySelector('.closeOne');
-closeOne.addEventListener('click', closeNav);
-const closeTwo = document.querySelector('.closeTwo');
-closeTwo.addEventListener('click', closeNav);
-const closeThree = document.querySelector('.closeThree');
-closeThree.addEventListener('click', closeNav);
-const closeFour = document.querySelector('.closeFour');
-closeFour.addEventListener('click', closeNav);
-const closeFive = document.querySelector('.closeFive');
-closeFive.addEventListener('click', closeNav);
-const closeSix = document.querySelector('.closeSix');
-closeSix.addEventListener('click', closeNav);
+const closeOne = document.querySelector(".closeOne");
+closeOne.addEventListener("click", closeNav);
+const closeTwo = document.querySelector(".closeTwo");
+closeTwo.addEventListener("click", closeNav);
+const closeThree = document.querySelector(".closeThree");
+closeThree.addEventListener("click", closeNav);
+const closeFour = document.querySelector(".closeFour");
+closeFour.addEventListener("click", closeNav);
+const closeFive = document.querySelector(".closeFive");
+closeFive.addEventListener("click", closeNav);
+const closeSix = document.querySelector(".closeSix");
+closeSix.addEventListener("click", closeNav);
 
-$('.load').slice(0, 2).show();
-$('.btn--classic__load').on('click', () => {
-  $('.load:hidden').slice(0, 2).slideDown();
-  if ($('.load:hidden').length === 0) {
-    $('.btn--classic__load').fadeOut('slow');
+// Initially show the first two items
+$(".load").slice(0, 2).show();
+$(".btn--classic__load--less").hide();
+$(".btn--classic__load").on("click", () => {
+  const hiddenLoads = $(".load:hidden");
+
+  // If there are hidden items, show two more on each click
+  if (hiddenLoads.length > 0) {
+    hiddenLoads.slice(0, 2).slideDown();
+
+    // If all items are shown, hide the "See More" button
+    if (hiddenLoads.length <= 2) {
+      $(".btn--classic__load").fadeOut("slow");
+      $(".btn--classic__load--less").show();
+    }
+  }
+});
+$(".btn--classic__load--less").on("click", (event) => {
+  event.preventDefault()
+  const visibleElements = $('.load:visible');
+  const elementsToHide = visibleElements.slice(-2); // Hide the last two visible elements
+  
+  elementsToHide.slideUp();
+  $('html, body').animate({ scrollTop: $('.load').offset().top }, 'slow');
+  if (visibleElements.length <= 4) {
+    $('.btn--classic__load').fadeIn('slow');
+    $(".btn--classic__load--less").hide()
   }
 });
